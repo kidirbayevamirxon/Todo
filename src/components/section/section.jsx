53a962ec0514297e8a98cf38fb9906a0ffd86ea6
kidirbayevamirxon <amirxonkidirbayev0@gmail.com>
2025-01-12 23:30:46 +0500

    fdg

--- src/components/section/section.jsx
+++ src/components/section/section.jsx
@@ -1,25 +1,25 @@
-import classes from './section.module.css';
-import { Button } from '../button/button.jsx';
-import strelka from '../../assets/arrow-right-solid.svg';
+import classes from "./section.module.css";
+import { Button } from "../button/button.jsx";
+import strelka from "../../assets/arrow-right-solid.svg";
 export function Section() {
-        const input=document.querySelector('.sectionInput');
-        const text=document.querySelector('.text');
-        const btn=document.querySelector('.btnImg');
-        // btn.addEventListener('click',()=>{
-        //     text.textContent=input.value;
-        // })
 
-    return (
-        <>
-            <section className={classes.section}>
-               <div className={classes.sectionDiv}>
-                <input type="text" className={classes.sectionInput} placeholder='Try typing Buy milk'/>
-                <Button ><img src={strelka} alt="" className={classes.btnImg} /></Button>
-               </div>
-               <div className={classes.textDiv}>
-                <p className={classes.text}></p>
-               </div>
-            </section>
-        </>
-    )
+  return (
+    <>
+      <section className={classes.section}>
+        <div className={classes.sectionDiv}>
+          <input
+            type="text"
+            className={classes.sectionInput}
+            placeholder="Try typing Buy milk"
+          />
+          <Button>
+            <img src={strelka} alt="" className={classes.btnImg} />
+          </Button>
+        </div>
+        <div className={classes.textDiv}>
+          <p className={classes.text}></p>
+        </div>
+      </section>
+    </>
+  );
 }
